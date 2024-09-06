d15afac4c0cec4f0f9f72fd3443d352337272fca
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-09-06 14:19:31 -0500

    Update 9/6

--- prog_54b.gd
+++ prog_54b.gd
@@ -1,11 +1,22 @@
 extends Control
 
+func _on_btn_calc_pressed() -> void:
+	var val1 = int($txtNum1.text)
+	var val2 = int($txtNum2.text)
+	var val3 = int($txtNum3.text)
+	var val4 = int($txtNum4.text)
+	var sum = int(val1+val2+val3+val4)
+	var avg = int((val1+val2+val3+val4)/4)
+	$lblSum.text = "Sum: " + str(sum)
+	$lblAvg.text = "Average: " + str(avg)
+	
+func _on_btn_clear_pressed() -> void:
+	$lblSum.text = "Sum: "
+	$lblAvg.text = "Average: "
+	$txtNum1.text = ""
+	$txtNum2.text = ""
+	$txtNum3.text = ""
+	$txtNum4.text = ""
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
