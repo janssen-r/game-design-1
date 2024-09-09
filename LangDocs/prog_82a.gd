04f8265ddc2747ff834b48bc86599f82fa262824
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-09-09 14:10:39 -0500

    Update 9/9

---
+++ LangDocs/prog_82a.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _on_btn_calc_pressed() -> void:
+	var speedLimit = int($txtLimit.text)
+	var carSpeed = int($txtSpeed.text)
+	var milesOver = carSpeed-speedLimit
+	var fine = 20.0 + (milesOver * 5.0)
+	$lblTotal.text = "$ %.2f" % fine # Rounds fine to 2 decimal pts
+	
+
+func _on_btn_clear_pressed() -> void:
+	$lblFine.text = "$"
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
