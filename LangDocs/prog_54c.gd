e0d091391a116841d14e20c7ed216daa1da2b181
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-09-09 14:33:20 -0500

    Update 9/9

--- LangDocs/prog_54c.gd
+++ LangDocs/prog_54c.gd
@@ -3,8 +3,8 @@ extends Control
 func _on_btn_calc_pressed() -> void:
 	const pi = float(3.14159)
 	var rad = float($txtRad.text)
-	var circum = float(2 * pi * rad)
-	var area = float((pi * rad)**2)
+	var circum = (2 * pi * rad)
+	var area = (pi * (rad**2))
 	$lblArea.text = "Area: " + str(area)
 	$lblCirc.text = "Circumference: " + str(circum)
 
