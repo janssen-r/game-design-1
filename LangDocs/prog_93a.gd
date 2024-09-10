b65b1ac0dbbc2c8eeb1d27034366d5f60c1ba2de
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-09-10 14:34:28 -0500

    Update 9/10

---
+++ LangDocs/prog_93a.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_btn_calc_pressed() -> void:
+	var Kwh = int($txtKwh.text)
+
+
+func _on_btn_calc_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_calc_3_pressed() -> void:
+	pass # Replace with function body.
