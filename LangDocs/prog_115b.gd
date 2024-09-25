433033ff58b8adb371f58bf4c47831fa13caa007
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-09-25 14:23:48 -0500

    Update 9/25

---
+++ LangDocs/prog_115b.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_btn_calc_pressed() -> void:
+	var val = 2
+	
+
+func _on_btn_2_clear_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_btn_3_exit_pressed() -> void:
+	pass # Replace with function body.
