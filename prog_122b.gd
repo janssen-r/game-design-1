0515af7f497d993b62a771d9d432a1b5e88364b8
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-10-04 11:39:35 -0500

    Update 10/4

--- prog_122b.gd
+++ prog_122b.gd
@@ -1,16 +1,25 @@
 extends Control
 
 func _on_btn_calc_pressed() -> void:
-	var hours = 4
-	hours = str(4)
+	var pay = 4
+	var hours = 1
+	pay = str(4)
+	hours = str(1)
 	for num in range(1,41):
-		hours = int(hours)
-		$ItemList.add_item (hours)
+		pay = str(pay)
+		$ItemList.add_item (pay)
+		pay = int(pay)
+		pay += 4
+
+	for num in range (1,41):
 		hours = str(hours)
-		hours += 4
+		$ItemListHours.add_item (hours)
+		hours = int(hours)
+		hours += 1
 
 func _on_btn_clear_pressed() -> void:
-	pass # Replace with function body.
+	$ItemList.clear()
+	$ItemListHours.clear()
 
 func _on_btn_exit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
