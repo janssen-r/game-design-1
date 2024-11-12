55c0759ae12557cc9ec42baabb7ed91652316514
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-11-12 14:31:28 -0600

    Update 11/12

--- td_enemy.gd
+++ td_enemy.gd
@@ -40,7 +40,7 @@ var ai_timer_max = 0.5
 var ai_timer = ai_timer_max - randi() % 5
 var damage_lock = 0.0
 var animation_lock = 0.0
-var knockback = 128.0
+var knockback = 128.0 * 1.25
 var vision_distance = 50.0
 var money_value = 5.0
 
