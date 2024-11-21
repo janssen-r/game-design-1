7ed130e2b13252b420dece78a2f8378cef2debed
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2024-11-21 14:36:35 -0600

    Update 11/21

--- td_player.gd
+++ td_player.gd
@@ -113,12 +113,12 @@ func _physics_process(delta: float) -> void:
 		if data.state != STATES.CHARGING:
 			data.state = STATES.IDLE
 			
-	if Input.is_action_just_pressed("ui_select"): #Enter
-		for entity in get_tree().get_nodes_in_group("Interactable"):
-			if entity.in_range(self):
-				entity.interact(self)
-				data.state = STATES.IDLE
-				return
+		if Input.is_action_just_pressed("ui_select"): #Enter
+			for entity in get_tree().get_nodes_in_group("Interactable"):
+				if entity.in_range(self):
+					entity.interact(self)
+					data.state = STATES.IDLE
+					return
 	
 		var direction = Vector2(
 			Input.get_axis("ui_left", "ui_right"),
